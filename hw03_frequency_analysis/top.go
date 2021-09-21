c8aefb32f14e1b5d2fc1547c96757b88596359ff
dingowd <dingowd75@yandex.ru>
2021-09-21 21:57:48 +0500

    hw03_frequency_analysis completed attempt 2

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -10,45 +10,49 @@ type wStruct struct {
 	word string
 }
 
+//func for structure sorting
 func sortSlice(slice []wStruct) []wStruct {
-	sort.SliceStable(slice, func(i, j int) bool { return slice[i].word < slice[j].word })
-	sort.SliceStable(slice, func(i, j int) bool { return slice[i].num > slice[j].num })
+	sort.SliceStable(slice, func(i, j int) bool {
+		if slice[i].num != slice[j].num {
+			return slice[i].num > slice[j].num
+		}
+		return slice[i].word < slice[j].word
+	})
 	return slice
 }
 
 func Top10(inStr string) []string {
+	//return empty slice if string is empty
 	if inStr == "" {
 		return nil
 	}
-	wE := wStruct{}
-	words := []wStruct{}
-	var n int
+	//get work slice
 	workSlice := strings.Fields(inStr)
-	outSlice := []string{}
+	//init and fill work map
 	wMap := make(map[string]int)
 	for i := range workSlice {
-		wMap[workSlice[i]] = 0
+		wMap[workSlice[i]]++
 	}
-	for i := range workSlice {
-		if _, inMap := wMap[workSlice[i]]; inMap {
-			wMap[workSlice[i]]++
-		}
-	}
+	//init and fill structure to sort
+	words := make([]wStruct, len(wMap))
 	for key, val := range wMap {
+		wE := wStruct{}
 		wE.word = key
 		wE.num = val
 		words = append(words, wE)
 	}
+	//sort structure
 	words = sortSlice(words)
+	n := 10
 	if len(words) < 10 {
 		n = len(words)
-	} else {
-		n = 10
 	}
-	sorted := words[:n]
-	sorted = sortSlice(sorted)
-	for i := range sorted {
-		outSlice = append(outSlice, sorted[i].word)
+	//cut sorted structure
+	words = words[:n]
+	//get slice to return
+	outSlice := []string{}
+	for i := range words {
+		outSlice = append(outSlice, words[i].word)
 	}
 	return outSlice
 }
